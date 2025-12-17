dbf836665b40df909eca0f668694340e7918a235
Thanakorns <>
2025-12-17 15:51:01 +0700

    fix error

--- app/src/main/java/com/thans/answernote/presenter/ui/components/QuestionItem.kt
+++ app/src/main/java/com/thans/answernote/presenter/ui/components/QuestionItem.kt
@@ -1,12 +1,21 @@
 package com.thans.answernote.presenter.ui.components
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.thans.answernote.model.Answer
 
 @Composable
 fun QuestionItem(
